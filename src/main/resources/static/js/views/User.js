0b2ee658bc1a7f8081f7be8c82896900ee1fd3cf
Amado Azua III <amado.azua3@gmail.com>
2021-08-18 13:36:11 -0500

    Spring user controllers

--- src/main/resources/static/js/views/User.js
+++ src/main/resources/static/js/views/User.js
@@ -6,35 +6,35 @@ export default function User(props){
         <header>
         <h1>User information</h1>
         </header>
-        <form>
-        <div class="row g-3">
-            <div class="col">
-            <input type="text" class="form-control" data-id=${user.id} aria-label="First name">
-            </div>
-        </div>
-        <div class="row g-3">
-            <div class="col">
-            <input type="text" class="form-control" data-id=${user.username} aria-label="First name">
-            </div>  
-        </div>  
-        <div class="row g-3">
-            <div class="col">
-            <input type="text" class="form-control" data-id=${user.email} aria-label="First name">
-            </div>
-        </div>
-        <div class="row g-3">
-            <div class="col">
-            <input type="text" class="form-control" placeholder="Password" aria-label="First name">
-            </div>
-        </div>
-            <button type="button" id="edit-btn">Edit</button>
+        
             
             ${props.user.map(post => `
-            
-            
+                <form>
+                    <div class="row g-3">
+                        <div class="col">
+                            <input type="text" class="form-control" data-id=${user.id} aria-label="First name">
+                        </div>
+                    </div>
+                    <div class="row g-3">
+                        <div class="col">
+                            <input type="text" class="form-control" data-id=${user.username} aria-label="First name">
+                        </div>
+                    </div>
+                    <div class="row g-3">
+                        <div class="col">
+                            <input type="text" class="form-control" data-id=${user.email} aria-label="First name">
+                        </div>
+                    </div>
+                    <div class="row g-3">
+                        <div class="col">
+                            <input type="text" class="form-control" placeholder="Password" aria-label="First name">
+                        </div>
+                    </div>
+                    <button type="button" id="edit-btn">Edit</button>
+                </form>
             `)}
 
-        </form>
+
 
     `
 }
